09c4e3f54096b688bf3bc2eb190218fc7fa57949
Ennis Lynch <elynch@orizon360.com>
2017-08-22 09:04:45 -0400

    Updated with comments
    Added comments in a code review in gracious consideration of the time you took to submit a code sample as part of the application process.

--- war_game_simulator/Program.cs
+++ war_game_simulator/Program.cs
@@ -15,6 +15,10 @@ namespace war_game_simulator
             var p1 = new Player(gowDeck);
             var p2 = new Player(gowDeck);
 
+            //There is no application logic. 
+            //This seems to be an infnite loop
+            //also because of the error in the constructors
+            //above this code is never reached
             while(!gowDeck.IsEmpty())
             {
 
@@ -34,13 +38,13 @@ namespace war_game_simulator
 
             cards = new Stack<Card>();
 
-            
+            //Innovative way to create all the cards
             foreach (var cv in Enum.GetValues(typeof(suits)).Cast<suits>().Zip(Enumerable.Range(1, 13), (a, b) => new { sv = a, rv = b }))
             {
                 cards.Push(new Card(cv.sv, cv.rv));
             }
 
-
+            
             cards.Shuffle();
           
 
@@ -54,13 +58,16 @@ namespace war_game_simulator
 
         public Card GetTopCard()
         {
-
+            //Not neccessary to compare a boolean 
+            //if(IsEmpty()) works too
             if (IsEmpty() == false)
             {
                 return cards.Pop();
 
             }
-
+            //Is this an exception or is this a normal
+            //state of application operation? How is this exception 
+            //handled?
             throw new Exception("Empty!");
 
         }
@@ -83,7 +90,8 @@ namespace war_game_simulator
         }
 
         public suits Suite { get; set; }
-
+        //What happens if the player implementation changes the 
+        //rank?
         public int Rank { get; set; }
 
 
@@ -96,11 +104,10 @@ namespace war_game_simulator
         {
             return c1.Rank != c2.Rank;
         }
-
+        ///Are these operators used in the application?
         public static bool operator <(Card c1, Card c2)
         { return c1.Rank < c2.Rank; }
-
-
+        ///Are these operators used in the application?
         public static bool operator >(Card c1, Card c2)
         { return c1.Rank > c2.Rank; }
 
@@ -110,8 +117,12 @@ namespace war_game_simulator
 
     class Player
     {
+        ///This constructor trhows an exception
         public Player(Deck dk)
         {
+            //Do players grab the top 26 cards or are the 
+            //cards dealt round robin? How do we know 
+            //how many cards are in the Deck
             for (int i = 0; i < 26; i++)
             {
                 var temp = dk.GetTopCard();
@@ -123,12 +134,16 @@ namespace war_game_simulator
 
         public Card DrawACard()
         {
+            //Is is necessary to shuffle the cards
+            //for each draw?
             playerCards.Shuffle();
             return playerCards.Pop();
         }
-
+        ///Playercards is never initialized
+        ///Usually Properties in .NET would be PascalCase
        public Stack<Card> playerCards { get; set; }
-
+        ///What is points_accumulated? This is a privater
+        ///member that is not referecend
        private int points_accumulated;
     
     }
@@ -136,11 +151,15 @@ namespace war_game_simulator
 
     public static class WarGameExtensions
     {
+        ///Does shuffle belong as a method in Deck or as en extension method?
         public static void Shuffle<Card>(this Stack<Card> cards)
         {
+            //Can the shuffle be done inplace or is a clone
+            //of the array necessary?
             var cardValues = cards.ToArray();
             cards.Clear();
-
+            //What does the documentation for Random read
+            //in regards to the default constructor?
             var rnd = new Random(DateTime.Now.Millisecond);
 
             foreach (var cv in cardValues.OrderBy(x => rnd.Next(cards.Count)))
