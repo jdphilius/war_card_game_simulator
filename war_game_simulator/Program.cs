09c497b43bea4dc64928b8cd5b880f87af70e656
jdphilius <philiusjd@gmail.com>
2017-08-21 21:56:51 -0400

    initial commit

---
+++ war_game_simulator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace war_game_simulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
